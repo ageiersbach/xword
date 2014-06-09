d1784d8198701551a7004e5ce1182f5084b5d8d2
ageiersbach <annasazi@gmail.com>
2014-06-09 10:44:27 -0400

    Created static page for application#index
    * Stretch-goal: Move angular functions to puzzles#new template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Xword::Application.routes.draw do
-  root :to => 'puzzles#new'
+  root :to => 'application#index' 
+  
+  #stretch goal: 
+  #root :to => 'puzzles#new'
   resources :puzzles
   
   namespace :api, defaults: { format: :json } do
