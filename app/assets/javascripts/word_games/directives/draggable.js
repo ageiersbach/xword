12048add66c0a35b8763397301913232d5a35c1d
ageiersbach <annasazi@gmail.com>
2014-06-09 23:21:58 -0400

    Add draggable directive

---
+++ app/assets/javascripts/word_games/directives/draggable.js
@@ -0,0 +1,30 @@
+angular
+  .module('myTinyApp')
+  .directive('draggable', function() {
+    return function(scope, element) {
+      var el;
+      el = element[0];
+      el.draggable = true;
+      
+      el.addEventListener(
+        'dragstart',
+        function(e) {
+          e.dataTransfer.effectAllowed = 'move';
+          e.dataTransfer.setData('Text', this.id);
+          this.classList.add('drag');
+          return false;
+        },
+        false
+      );
+
+      el.addEventListener(
+        'dragend',
+        function(e) {
+          this.classList.remove('drag');
+          return false;
+        },
+        false
+      );
+    }
+  });
+
